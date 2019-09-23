90b1ce730581cf4badf983f60dc5e4707411f2db
pau-lo <pau_lo_lop_ez@protonmail.com>
2019-09-23 16:59:00 -0700

    added sessions and password hash

--- gorilla-mux-example/08-adding-password/main.go
+++ gorilla-mux-example/08-adding-password/main.go
@@ -5,6 +5,8 @@ import (
 	"html/template"
 	"net/http"
 
+	"golang.org/x/crypto/bcrypt"
+
 	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
@@ -33,12 +35,12 @@ func main() {
 	// and change it to helloHandler
 	r.HandleFunc("/", indexGetHandler).Methods("GET")
 	r.HandleFunc("/", indexPostHandler).Methods("POST")
-
+	// adding login for get and post
 	r.HandleFunc("/login", loginGetHandler).Methods("GET")
 	r.HandleFunc("/login", loginPostHandler).Methods("POST")
-
-	r.HandleFunc("/test", testGetHandler).Methods("GET")
-
+	// adding to new handlers for registering new users
+	r.HandleFunc("/register", registerGetHandler).Methods("GET")
+	r.HandleFunc("/register", registerPostHandler).Methods("POST")
 	// instantiate a file server object for file handler
 	// tell it which directory to get our files from
 	fs := http.FileServer(http.Dir("./static/"))
@@ -51,8 +53,15 @@ func main() {
 
 // hello Handler
 func indexGetHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+	_, ok := session.Values["username"]
+	if !ok {
+		http.Redirect(w, r, "/login", 302)
+	}
 	comments, err := client.LRange("comments", 0, 10).Result()
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
 		return
 	}
 	templates.ExecuteTemplate(w, "index.html", comments)
@@ -61,9 +70,14 @@ func indexGetHandler(w http.ResponseWriter, r *http.Request) {
 func indexPostHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	comment := r.PostForm.Get("comment")
-	client.LPush("comments", comment)
-	// redirect client back to our page
+	err := client.LPush("comments", comment)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
 	http.Redirect(w, r, "/", 302)
+
 }
 
 func loginGetHandler(w http.ResponseWriter, r *http.Request) {
@@ -74,21 +88,59 @@ func loginPostHandler(w http.ResponseWriter, r *http.Request) {
 	// grab the name they submitted
 	r.ParseForm()
 	username := r.PostForm.Get("username")
+	password := r.PostForm.Get("password")
+	hash, err := client.Get("user:" + username).Bytes()
+	if err == redis.Nil {
+		templates.ExecuteTemplate(w, "login.html", "unknown user")
+		return
+
+	} else if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+
+	}
+
+	err = bcrypt.CompareHashAndPassword(hash, []byte(password))
+	if err != nil {
+		templates.ExecuteTemplate(w, "login.html", "Invalid login")
+		return
+	}
+	// creating the session
 	session, _ := store.Get(r, "session")
 	session.Values["username"] = username
 	session.Save(r, w)
+	http.Redirect(w, r, "/", 302)
 
 }
 
-func testGetHandler(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "session")
-	untyped, ok := session.Values["username"]
-	if !ok {
+func registerGetHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "register.html", nil)
+
+}
+
+func registerPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username := r.PostForm.Get("username")
+	password := r.PostForm.Get("password")
+	// using bcrypt define a cost parameter using the default value
+	cost := bcrypt.DefaultCost
+	// generating the hash value while bcrypt exptects our password to be in byte
+	// form but we will convert it to a string
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
 		return
 	}
-	username, ok := untyped.(string)
-	if !ok {
+
+	err = client.Set("user:"+username, hash, 0).Err()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
 		return
 	}
-	w.Write([]byte(username))
+
+	http.Redirect(w, r, "/login", 302)
 }
